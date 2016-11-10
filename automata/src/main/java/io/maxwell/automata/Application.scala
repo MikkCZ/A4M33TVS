cd5d09d8b80c4e4dbb94bdbefe8437d73cbadadd
Marek Timr <timrmare@fel.cvut.cz>
2016-11-10 21:21:28 +0100

    generate output for given sequence of events

--- automata/src/main/java/io/maxwell/automata/Application.scala
+++ automata/src/main/java/io/maxwell/automata/Application.scala
@@ -10,5 +10,13 @@ object Application {
     val settings = ConfigFactory.load().getConfig("automata")
     val inputFileName = settings.getString("input-csv")
     val automata = new AutomataParser(inputFileName).getAutomata
+
+    print(automata.output(
+      List(
+        Event("e38"),
+        Event("e08"),
+        Event("e20"),
+        Event("e20"),
+        Event("e38"))))
   }
 }
--- automata/src/main/java/io/maxwell/automata/Automata.scala
+++ automata/src/main/java/io/maxwell/automata/Automata.scala
@@ -5,4 +5,17 @@ package io.maxwell.automata
   */
 class Automata(val entryNode: Node, val exitNode: Node, val defaultOutput: Output, val nodes: Map[String, Node]) {
 
+  def output(input: List[Event]): List[Output] = {
+    def step(node: Node, input: List[Event], output: List[Output]): List[Output] = {
+      input match {
+        case e::rest =>
+          val transition = node.transitions.find(t => t.event == e).get //je to DFA
+          step(transition.target,
+              rest,
+              output :+ transition.output)
+        case _ => output
+      }
+    }
+    step(entryNode, input, List.empty)
+  }
 }
