3a74c82e20849fa99eef78b3699d0b8634799f84
Michal Stanke <michal@stanke.cz>
2016-11-29 22:13:38 +0100

    StateCoverageFinder.scala cleanup

--- automata/src/main/java/io/maxwell/automata/StateCoverageFinder.scala
+++ automata/src/main/java/io/maxwell/automata/StateCoverageFinder.scala
@@ -8,7 +8,6 @@ import scala.collection.mutable
   * @author Vorisek Lukas <lukasvorisek@gmail.com>
   */
 class StateCoverageFinder(automata: Automata, events: Array[Event]) {
-  private var lastFindMapping: mutable.Map[String, List[Event]] = _
 
   def find() = {
     val nodes: mutable.MutableList[Node] = mutable.MutableList.empty
@@ -20,41 +19,25 @@ class StateCoverageFinder(automata: Automata, events: Array[Event]) {
     nodeMap(automata.entryNode.name) = List.empty
     outputSet += List.empty
     // Scan all nodes
-    // Nevim proc, ale Scala zapis mi prosel jen 3 elementy a to i s iteratorem
-    //nodes.foreach(node => scan(node, nodes, nodeMap))
     JavaHelpers.forAll[Node](nodes, new DoCommand[Node] {
       override def doCommand(element: Option[Node], index: Int) = {
         scan(element.get, nodes, nodeMap, outputSet)
       }
     })
 
-//    automata.nodes.keys.foreach {
-//      nodeKey =>
-//        val node = automata.nodes(nodeKey)
-//        println(node + " way " + nodeMap(node.name))
-//    }
-
-    lastFindMapping = nodeMap
     outputSet
-  }
-
-  def findMapping() = {
-    if(lastFindMapping == null) {
-      find()
-    }
-    lastFindMapping
   }
 
   def scan(node: Node, nodes: mutable.MutableList[Node], nodeMap: mutable.Map[String,List[Event]], outputSet: mutable.Set[List[Event]]) = {
     // Pro kazdy dostupny event
     events.foreach {
-      ev => {
-        val trans = node.transitions(ev)
-        if(trans != null) {
-          if(!nodes.contains(trans.target)) {
-            nodes += trans.target
-            nodeMap(trans.target.name) = nodeMap(node.name) :+ ev
-            outputSet += (nodeMap(node.name) :+ ev)
+      event => {
+        val transition = node.transitions(event)
+        if(transition != null) {
+          if(!nodes.contains(transition.target)) {
+            nodes += transition.target
+            nodeMap(transition.target.name) = nodeMap(node.name) :+ event
+            outputSet += (nodeMap(node.name) :+ event)
           }
         }
       }
